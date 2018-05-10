ea7789f2204d2b36f015778935422fb80c0e9532
Felix Naumann <felix@emilnaumann.de>
2018-05-10 11:02:38 +0200

    changed redundant code / reformatted code

--- src/dd.c
+++ src/dd.c
@@ -17,104 +17,106 @@ FILE *input;
 FILE *output;
 
 char *getpathofarg( char *string );
+
 void usage( char *prgname );
 
 //TODO: free all string pointers, close files in all cases
 
 int main( int argc, char **argv ) {
-		//Check if input and output are given
-		//and input leads to an existing file
-		int inputgiven = 0;
-		int outputgiven = 0;
+    //Check if input and output are given
+    //and input leads to an existing file
+    int inputgiven = 0;
+    int outputgiven = 0;
 
-		for (int i = 1; i < argc; i++) {
-			if(strstr(argv[i], "if=")) {
-				if (!inputgiven) {
-					inputgiven = 1;
-					input = fopen(getpathofarg(argv[i]), "rb");
+    for (int i = 1; i < argc; i++) {
+        if (strstr(argv[i], "if=")) {
+            if (!inputgiven) {
+                inputgiven = 1;
+                input = fopen(getpathofarg(argv[i]), "rb");
 
-					//Check if user is permitted to read the input file
-					if (input == NULL) {
-						if (errno == EACCES) {
-							fputs("Access to input file denied, please rerun command as a privileged user!\n", stderr);
-						} else {
-							fputs("Input file doesn't exist!\n", stderr);
-						}
-						return 1;
-					}
+                //Check if user is permitted to read the input file
+                if (input == NULL) {
+                    if (errno == EACCES) {
+                        fputs("Access to input file denied, please rerun command as a privileged user!\n", stderr);
+                    } else {
+                        fputs("Input file doesn't exist!\n", stderr);
+                    }
+                    return 1;
+                }
 
-				} else {
-					fputs("Too many input files!", stderr);
-					return 1;
-				}
-			}
+            } else {
+                fputs("Too many input files!", stderr);
+                return 1;
+            }
+        }
 
-			if (strstr(argv[i], "of=")) {
-				if (!outputgiven) {
-					outputgiven = 1;
-					output = fopen(getpathofarg(argv[i]), "wb");
+        if (strstr(argv[i], "of=")) {
+            if (!outputgiven) {
+                outputgiven = 1;
+                output = fopen(getpathofarg(argv[i]), "wb");
 
-					//Check if user is permitted to write to the output file
-					if (output == NULL) {
-						if (errno == EACCES) {
-							fputs("Access to output file denied, please rerun command as a privileged user!\n", stderr);
-							return 1;
-						}		
-					}
-				} else {
-					fputs("Only one output is possible!\n", stderr);
-					return 1;
-				}
-			}
-		}
+                //Check if user is permitted to write to the output file
+                if (output == NULL) {
+                    if (errno == EACCES) {
+                        fputs("Access to output file denied, please rerun command as a privileged user!\n", stderr);
+                        return 1;
+                    }
+                }
+            } else {
+                fputs("Only one output is possible!\n", stderr);
+                return 1;
+            }
+        }
+    }
 
-		if (!inputgiven) {
-			input = stdin;
-		}
-		
-		if(!outputgiven) {
-			output = stdout;
-		}
+    if (!inputgiven) {
+        input = stdin;
+    }
 
-		int c = 0;
-		
-		while(c != EOF) {
-			c = fgetc(input);
-			if (c == EOF) {
-				break;
-			}
+    if (!outputgiven) {
+        output = stdout;
+    }
 
-			//Check if in- and output are stdin and stdout and they're not piped
-			if((!isatty(STDIN_FILENO) || input != stdin) || (!isatty(STDOUT_FILENO) || output != stdout)) {
-				fputc(c, output);
-			}
-		}
+    int c = 0;
 
-		fclose(input);
-		fclose(output);
+    while (1) {
+        c = fgetc(input);
 
-		fputs("Complete!\n", stdout);
+        if (c == EOF) {
+            break;
+        }
 
-		return 0;
+        //Check if in- and output are stdin and stdout and they're not piped
+        if ((!isatty(STDIN_FILENO) || input != stdin) || (!isatty(STDOUT_FILENO) || output != stdout)) {
+            fputc(c, output);
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+
+    fputs("Complete!\n", stdout);
+
+    return 0;
 
 }
 
 void usage( char *prgname ) {
-	printf("usage: %s if=/path/to/file of=/path/to/file\n", prgname);
+    printf("usage: %s if=/path/to/file of=/path/to/file\n", prgname);
 }
 
 char *getpathofarg( char *string ) {
-	char *temp = malloc(sizeof(char) * (strlen(string) - 2));
+    char *temp = malloc(sizeof(char) * (strlen(string) - 2));
 
-	int j = 0;
+    int j = 0;
 
-	//Copy filepath into a new string without if= or of= 
-	for (int i = 3; i < strlen(string); i++) {
-		temp[j] = string[i];
-		j++;
-	}
+    //Copy filepath into a new string without if= or of=
+    for (int i = 3; i < strlen(string); i++) {
+        temp[j] = string[i];
+        j++;
+    }
 
-	temp[strlen(string) - 3] = '\0';
+    temp[strlen(string) - 3] = '\0';
 
-	return temp;
+    return temp;
 }
